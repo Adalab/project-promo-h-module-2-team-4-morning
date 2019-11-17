ec8b0860f39273b69e419525cede1b645ab9112d
María José <mjchirinos@gmail.com>
2019-11-17 18:39:32 +0100

    add button reset functionality

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,16 @@
-'use strict';
+"use strict";
 
-console.log('>> Ready :)');
+console.log(">> Ready :)");
+
+/**
+ * Reset del formulario
+ */
+
+let form = document.querySelector("form");
+const resetFormBtn = document.querySelector(".preview__reset");
+
+function resetForm() {
+  form.reset();
+}
+
+resetFormBtn.addEventListener("click", resetForm);
