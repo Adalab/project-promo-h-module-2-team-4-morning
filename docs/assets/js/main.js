9e9bcfee9437f448165454641e2ad7b8a4e82d5c
Isabel del R <i.beldelreal@gmail.com>
2019-11-17 19:10:40 +0100

    Update docs

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -1,1 +1,1 @@
-"use strict";alert(">> Ready :)");let form=document.querySelector("form");const resetFormBtn=document.querySelector(".preview__reset");function resetForm(){form.reset()}resetFormBtn.addEventListener("click",resetForm);
+"use strict";console.log(">> Ready :)");let form=document.querySelector("form");const resetFormBtn=document.querySelector(".preview__reset");function resetForm(){form.reset()}resetFormBtn.addEventListener("click",resetForm);
