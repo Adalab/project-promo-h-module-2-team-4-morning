082cd0e43eefed34c0b4e7a92b7fae1276c07df0
María José <mjchirinos@gmail.com>
2019-11-18 13:58:29 +0100

    add js color palette options

--- _src/assets/js/design.js
+++ _src/assets/js/design.js
@@ -1,3 +1,37 @@
-'use strict';
+"use strict";
 
-console.log('prueba2');
+// Color palette 1 //
+
+const selectPalette1 = document.getElementById("color1");
+function changeColor1() {
+  let color = document.querySelector(".preview__display");
+  color.classList.remove("palette2");
+  color.classList.remove("palette3");
+  color.classList.add("palette1");
+}
+
+selectPalette1.addEventListener("click", changeColor1);
+
+/// Color palette 2 //
+
+const selectPalette2 = document.getElementById("color2");
+function changeColor2() {
+  let color = document.querySelector(".preview__display");
+  color.classList.remove("palette1");
+  color.classList.remove("palette3");
+  color.classList.add("palette2");
+}
+
+selectPalette2.addEventListener("click", changeColor2);
+
+// Color palette 3 //
+
+const selectPalette3 = document.getElementById("color3");
+function changeColor3() {
+  let color = document.querySelector(".preview__display");
+  color.classList.remove("palette1");
+  color.classList.remove("palette2");
+  color.classList.add("palette3");
+}
+
+selectPalette3.addEventListener("click", changeColor3);
