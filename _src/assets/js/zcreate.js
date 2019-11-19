f3f9b6dd02d43066c015ee07876bcd2a28c9a91e
elenapayan <elena.payan.glez@gmail.com>
2019-11-19 16:16:38 +0100

    Add eventListener to check if all inputs are validated

--- _src/assets/js/zcreate.js
+++ _src/assets/js/zcreate.js
@@ -1,3 +1,36 @@
 'use strict';
 
-console.log('prueba3');
+// const form = document.querySelector(".share__btn");
+
+// const checkForm = function () {
+//     return form.checkValidity();
+// };
+
+// const validateForm = function () {
+//     if (checkForm === true) {
+//         console.log('esto funciona');
+//     } else {
+//         console.log('ojito');
+//     }
+// };
+
+const beitaGuapa = document.querySelector('.js-elenaGuapa');
+const createAllowCard = document.querySelector('.js-share__btn')
+
+console.log(beitaGuapa.checkValidity())
+
+const checkCheck = function (event) {
+    if (beitaGuapa.checkValidity() === true) {
+        createAllowCard.classList.remove('js-filter');
+    } else {
+        createAllowCard.classList.add('js-filter');
+    };
+};
+// const formInputsArr = document.querySelectorAll(".fill__box");
+// console.log(formInputsArr);
+// console.log(fonmInputarrValue);
+
+inputName.addEventListener('keyup', checkCheck);
+// inputPosition.addEventListener('keyup', changePosition);
+// inputEmail.addEventListener('keyup', updateEmail);
+// inputGithub.addEventListener('keyup', updateGithub);
