b66ad57ede0c3e82f14af42c1c50a41d224263dd
Beatriz Sobrino Paredes <Beatrizsobri@github.com>
2019-11-25 19:50:12 +0100

    Finish input function

--- _src/assets/js/fill2.js
+++ _src/assets/js/fill2.js
@@ -3,34 +3,43 @@
 const inputArr = document.querySelectorAll('.js-input');
 const previewArr = document.querySelectorAll('.js-preview');
 
-function show (){
-    for (let i = 0; i< inputArr.length; i++) {
-        
-        inputArr[i].value;
-        console.log(inputArr[i].value);
-        
-        function updatePreview (){
-            const defaultPreviewArr=[];
-            defaultPreviewArr[0] ='Nombre y Apellido';
-            defaultPreviewArr[1] ='Front-End';
-            
-            if(i<2){
-            if (inputArr[i].value == false) {
-                previewArr[i].innerHTML= defaultPreviewArr[i];
-            }
-            else {
-            previewArr[i].innerHTML = inputArr[i].value;
-            }
-        } 
-        else {
-            console.log('esto me lo invento');
+function show() {
+  for (let i = 0; i < inputArr.length; i++) {
+
+    inputArr[i].value;
+    console.log(inputArr[i].value);
+
+    function updatePreview() {
+      const defaultPreviewArr = [];
+      defaultPreviewArr[0] = 'Nombre y Apellido';
+      defaultPreviewArr[1] = 'Front-End';
+      defaultPreviewArr[2] = '';
+      defaultPreviewArr[3] = '';
+      defaultPreviewArr[4] = '';
+      defaultPreviewArr[5] = '';
+
+      const hrefArr = [];
+      hrefArr[2] = 'mailto:';
+      hrefArr[3] = 'tel:+';
+      hrefArr[4] = 'https://www.linkedin.com/in/';
+      hrefArr[5] = 'https://www.github.com/';
+
+      if (i < 2) {
+        if (inputArr[i].value == false) {
+          previewArr[i].innerHTML = defaultPreviewArr[i];
+        } else {
+          previewArr[i].innerHTML = inputArr[i].value;
         }
-        
+      } else {
+        if (inputArr[i].value == false) {
+          previewArr[i].href = `${hrefArr[i]}${defaultPreviewArr[i]}`;
+        } else {
+          previewArr[i].href = `${hrefArr[i]}${inputArr[i].value}`;
+        }
+      }
     }
     inputArr[i].addEventListener('keyup', updatePreview);
-       
-}
-
+  }
 }
 show();
 
@@ -40,5 +49,3 @@ show();
 // const changePhone = document.querySelector('.js-link-phone');
 // const changeLinkedin = document.querySelector('.js-link-linkedin');
 // const changeGithub = document.querySelector('.js-link-github');
-
-
