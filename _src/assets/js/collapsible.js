e5a4a866f27bfe8186f6a11f44088b9fb1868fd6
María José <mjchirinos@gmail.com>
2019-11-25 12:05:57 +0100

    unify collapsible function

--- _src/assets/js/collapsible.js
+++ _src/assets/js/collapsible.js
@@ -1,42 +1,27 @@
 "use strict";
 
 const collapsibleArr = document.querySelectorAll(".collapsible");
+const fieldsetArr = document.querySelectorAll("fieldset");
 
-const collapsible1 = collapsibleArr[0];
-function show1() {
-  const article = document.querySelector(".design-box");
-  if (article.classList.contains("hidden")) {
-    article.classList.remove("hidden");
-    collapsible1.classList.add("open");
-  } else {
-    article.classList.add("hidden");
-    collapsible1.classList.remove("open");
+for (let i = 0; i < collapsibleArr.length; i++) {
+  collapsibleArr[i].addEventListener("click", toggleItem);
+  function toggleItem() {
+    toggle(i);
   }
 }
-collapsible1.addEventListener("click", show1);
 
-const collapsible2 = collapsibleArr[1];
-function show2() {
-  const article = document.querySelector(".fill");
-  if (article.classList.contains("hidden")) {
-    article.classList.remove("hidden");
-    collapsible2.classList.add("open");
+function toggle(itemIndex) {
+  const sel = fieldsetArr[itemIndex];
+  if (sel.classList.contains("hidden")) {
+    sel.classList.remove("hidden");
+    sel.classList.add("open");
   } else {
-    article.classList.add("hidden");
-    collapsible2.classList.remove("open");
+    sel.classList.add("hidden");
+    sel.classList.remove("open");
   }
 }
-collapsible2.addEventListener("click", show2);
 
-const collapsible3 = collapsibleArr[2];
-function show3() {
-  const article = document.querySelector(".share");
-  if (article.classList.contains("hidden")) {
-    article.classList.remove("hidden");
-    collapsible3.classList.add("open");
-  } else {
-    article.classList.add("hidden");
-    collapsible3.classList.remove("open");
-  }
-}
-collapsible3.addEventListener("click", show3);
+
+
+
+
