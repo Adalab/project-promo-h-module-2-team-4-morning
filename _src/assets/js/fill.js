7efae183a2cd9f97631eca0241434767889a08bd
Beatriz Sobrino Paredes <Beatrizsobri@github.com>
2019-11-26 12:15:53 +0100

    Update Reset and input function structure

--- _src/assets/js/fill.js
+++ _src/assets/js/fill.js
@@ -5,55 +5,100 @@ const previewArr = document.querySelectorAll('.js-preview');
 const formValidation = document.querySelector('.js-form-container');
 const createAllowCard = document.querySelector('.js-share__btn');
 
-function show() {
-  for (let i = 0; i < inputArr.length; i++) {
-    inputArr[i].value;
-    console.log(inputArr[i].value);
-
-    function checkCheck() {
-      if (formValidation.checkValidity() === true) {
-        createAllowCard.classList.remove('js-filter');
-      } else {
-        createAllowCard.classList.add('js-filter');
-      };
-    };
-
-    function updatePreview() {
-      const defaultPreviewArr = [
-        'Nombre Apellido',
-        'Front-End Developer',
-        '',
-        '',
-        '',
-        '',
-      ]
-      const hrefArr = [];
-      hrefArr[2] = 'mailto:';
-      hrefArr[3] = 'tel:+';
-      hrefArr[4] = 'https://www.linkedin.com/in/';
-      hrefArr[5] = 'https://www.github.com/';
-
-      if (i < 2) {
-        if (!!inputArr[i].value === false) {
-          previewArr[i].innerHTML = defaultPreviewArr[i];
-        } else {
-          previewArr[i].innerHTML = inputArr[i].value;
-        }
-      } else {
-        if (!!inputArr[i].value === false) {
-          previewArr[i].href = `${hrefArr[i]}${defaultPreviewArr[i]}`;
-        } else {
-          previewArr[i].href = `${hrefArr[i]}${inputArr[i].value}`;
-        }
-      }
-    };
-
-    function handler(event) {
-      updatePreview();
-      checkCheck();
-    };
-
-    inputArr[i].addEventListener('keyup', handler);
+function checkCheck() {
+  if (formValidation.checkValidity() === true) {
+    createAllowCard.classList.remove('js-filter');
+  } else {
+    createAllowCard.classList.add('js-filter');
   };
 };
-show();
+
+function listenInputs() {
+  for (let i = 0; i < inputArr.length; i++) {
+    inputArr[i].addEventListener('keyup', handler);
+  }
+};
+
+listenInputs();
+
+const defaultPreviewArr = [
+  'Nombre Apellido',
+  'Front-End Developer',
+  '',
+  '',
+  '',
+  '',
+]
+const hrefArr = [
+  false,
+  false,
+  'mailto:',
+  'tel:+',
+  'https://www.linkedin.com/in/',
+  'https://www.github.com/',
+]
+
+function updatePreview() {
+  for (let i = 0; i < inputArr.length; i++) {
+    if (i < 2) {
+      if (!!inputArr[i].value === false) {
+        previewArr[i].innerHTML = defaultPreviewArr[i];
+      } else {
+        previewArr[i].innerHTML = inputArr[i].value;
+      }
+    } else {
+      if (!!inputArr[i].value === false) {
+        previewArr[i].href = `${hrefArr[i]}${defaultPreviewArr[i]}`;
+      } else {
+        previewArr[i].href = `${hrefArr[i]}${inputArr[i].value}`;
+      }
+    }
+  };
+};
+
+function handler() {
+  // localstorage
+  updatePreview();
+  // updatePalette();
+  checkCheck();
+};
+
+// Image loading
+const fr = new FileReader();
+const uploadBtn = document.querySelector('.js-fill__image-btn');
+const fileField = document.querySelector('.js-fill__image-upload-btn');
+const profileImage = document.querySelector('.preview__display--img');
+const profilePreview = document.querySelector('.fill__image--miniature');
+
+
+function getImage(e) {
+  var myFile = e.currentTarget.files[0];
+  fr.addEventListener('load', writeImage);
+  fr.readAsDataURL(myFile);
+}
+/**
+ * Una vez tenemos los datos listos en el FR podemos
+ * trabajar con ellos ;)
+ */
+function writeImage() {
+  /* En la propiedad `result` de nuestro FR se almacena
+   * el resultado
+   */
+  profileImage.style.backgroundImage = `url(${fr.result})`;
+  profilePreview.style.backgroundImage = `url(${fr.result})`;
+}
+/**
+ * Genera un click automático en nuesto campo de tipo "file"
+ * que está oculto
+ */
+function fakeFileClick(e) {
+  e.preventDefault();
+  fileField.click();
+}
+/**
+ * Añadimos los listeners necesarios:
+ * - al botón visible para generar el click automático
+ * - al campo oculto para cuando cambie su value
+ */
+uploadBtn.addEventListener('click', fakeFileClick);
+fileField.addEventListener('change', getImage);
--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -7,16 +7,18 @@
 
 let form = document.querySelector("form");
 const resetFormBtn = document.querySelector(".preview__reset");
+const defaultImage = './assets/images/profile.png';
 
 function resetForm() {
   form.reset();
   document.querySelector(".preview__display").classList.remove("palette1");
   document.querySelector(".preview__display").classList.remove("palette2");
   document.querySelector(".preview__display").classList.remove("palette3");
-  checkCheck();
-  show();
-  updatePreview();
+  handler();
+
   // twitterActive();
+  profileImage.style.backgroundImage = `url(${defaultImage})`;
+  profilePreview.style.backgroundImage = `url('')`;
 }
 
 resetFormBtn.addEventListener("click", resetForm);
