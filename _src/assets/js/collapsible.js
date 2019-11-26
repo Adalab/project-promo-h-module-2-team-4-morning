65f7f0b3aae3932931df44a65878f88e20260d16
Beatriz Sobrino Paredes <Beatrizsobri@github.com>
2019-11-26 18:14:49 +0100

    Amend collapsible accordeon

--- _src/assets/js/collapsible.js
+++ _src/assets/js/collapsible.js
@@ -5,12 +5,16 @@ const fieldsetArr = document.querySelectorAll('fieldset');
 
 for (let i = 0; i < collapsibleArr.length; i++) {
   collapsibleArr[i].addEventListener('click', toggleItem);
+
   function toggleItem() {
     toggle(i);
   }
 }
 
 function toggle(itemIndex) {
+  fieldsetArr[0].classList.add('hidden');
+  fieldsetArr[1].classList.add('hidden');
+  fieldsetArr[2].classList.add('hidden');
   const sel = fieldsetArr[itemIndex];
   if (sel.classList.contains('hidden')) {
     sel.classList.remove('hidden');
