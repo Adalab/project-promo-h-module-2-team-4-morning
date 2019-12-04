0f66b0e2ecda0b487d78c5ff0cc41e25a3add333
Beatriz Sobrino Paredes <Beatrizsobri@github.com>
2019-12-04 11:33:38 +0100

    Amend arrow animation in collapsible

--- _src/assets/js/collapsible.js
+++ _src/assets/js/collapsible.js
@@ -14,15 +14,16 @@ for (let i = 0; i < collapsibleArr.length; i++) {
 
 function toggle(itemIndex) {
   const sel = fieldsetArr[itemIndex];
+  const col = collapsibleArr[itemIndex];
   const initialStatus = sel.classList.contains('hidden');
   fieldsetArr[0].classList.add('hidden');
   fieldsetArr[1].classList.add('hidden');
   fieldsetArr[2].classList.add('hidden');
   if (initialStatus) {
     sel.classList.remove('hidden');
-    sel.classList.add('open');
+    col.classList.add('open');
   } else {
     sel.classList.add('hidden');
-    sel.classList.remove('open');
+    col.classList.remove('open');
   }
 }
